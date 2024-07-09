533c86e6aa5a46562e242cc8d330ea6e6ad8b191
Caio Ferreira <caioferreirabcc@gmail.com>
2024-07-09 00:49:24 -0300

    Create white label structure

---
+++ cachoeira_ferreira/test/cachoeira_ferreira_test.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:cachoeira_ferreira/main.dart';
+
+void main() {}
---
+++ eco_track_whitelabel/test/eco_track_whitelabel_test.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:eco_track_whitelabel/main.dart';
+
+void main() {}
+
